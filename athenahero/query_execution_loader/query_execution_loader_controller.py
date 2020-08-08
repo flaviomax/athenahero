1318d606df7a9583219f99b49e2274da55f0394d
Flavio Altinier <flavio.altinier@pier.digital>
2020-08-08 18:13:51 -0300

    persist calls to query_execution_loader

--- athenahero/query_execution_loader/query_execution_loader_controller.py
+++ athenahero/query_execution_loader/query_execution_loader_controller.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, make_response
+import json
+from flask import Blueprint, make_response, request
 from athenahero import db
 from athenahero.database.models.query_execution import QueryExecution
 
@@ -8,12 +9,26 @@ query_execution_loader_bp = Blueprint(
     'query_execution_loader_bp', __name__,
 )
 
+def persist_query_execution(query_execution):
+    db.session.add(query_execution)
+    db.session.commit()
+
 @query_execution_loader_bp.route('/query_execution_loader', methods=['POST'])
 def query_execution_loader():
     """Persist one query execution object."""
+    # TODO: send this to a `utils` file 
+    request_data = json.loads(request.data)
 
-    headers = {"Content-Type": "application/json"}
-    return make_response(
-        'Test worked!',
+    # TODO: validate request
+    query_execution = QueryExecution(
+        id=request_data['id'],
+        query=request_data['id']
+    )
+    persist_query_execution(query_execution)
+
+    response = make_response(
+        {'message':'All Done!'},
         200,
     )
+    response.headers['Content-Type'] = "application/json"
+    return response
