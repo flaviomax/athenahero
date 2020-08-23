2c2330fed17c97343911839a97ff21759f6fbb15
Flavio Altinier <flavio.altinier@pier.digital>
2020-08-23 07:47:37 -0300

    schedule job only on production

--- athenahero/query_execution_loader/query_execution_loader.py
+++ athenahero/query_execution_loader/query_execution_loader.py
@@ -3,23 +3,24 @@ import atexit
 
 from apscheduler.schedulers.background import BackgroundScheduler
 from . import query_execution_loader_job
+from datetime import datetime, timedelta
+from flask import current_app
 
 def register_query_execution_job():
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(
-        name='last_30_days_updater',
-        func=query_execution_loader_job.populate_month_of_executions,
-        trigger="interval",
-        hours=6,
-        coalesce=True,
-        max_instances=1
-        )
-    scheduler.add_job(
-        name='last_30_days_update_immediately',
-        func=query_execution_loader_job.populate_month_of_executions,
-        coalesce=True
-    )
-    scheduler.start()
+    if current_app.config['ENV'] == 'production':
+        scheduler = BackgroundScheduler()
+        scheduler.add_job(
+            name='last_30_days_updater',
+            id='last_30_days_updater',
+            func=query_execution_loader_job.populate_month_of_executions,
+            trigger="interval",
+            hours=6,
+            coalesce=True,
+            max_instances=1,
+            next_run_time=datetime.now() + timedelta(seconds=3),
+            misfire_grace_time=30
+            )
+        scheduler.start()
 
-    # Shut down the scheduler when exiting the app
-    atexit.register(lambda: scheduler.shutdown(wait=False))
+        # Shut down the scheduler when exiting the app
+        atexit.register(lambda: scheduler.shutdown(wait=False))
